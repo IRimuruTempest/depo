57f156d5d8160349498df1fd0b5f94db7d7d9062
IRimuruTempest <32411356+IRimuruTempest@users.noreply.github.com>
2024-04-30 17:53:54 +0300

    Update hafta08.php

--- hafta08.php
+++ hafta08.php
@@ -1,50 +1,47 @@
+<?php
+session_start();
+
+if (isset($_POST['Adi'] , $_POST['Soyadi'], $_POST['Sifre'] )){
+    if ($_POST['Adi'] == 'Ali' , $_POST['Soyadi'] == 'Veli' , $_POST['Sifre'] == '1234') {
+    $_SESSION['Adi'] = $_POST['Adi'];
+    $_SESSION['Soyadi'] = $_POST['Soyadi'];
+
+    else {
+        $hata = "Ad , Soyad veye Sifre bilgisi hatalıdır ";
+    }
+
+    } else if (isset($_GET['Cikis'])){
+    session_destroy();
+    session_start();
+}
+}
+?>
 <html>
     <head>
 
     </head>
     <body>
 
-    <input type="text">u
         <?php
-        
+        if (isset($hata)){
+            echo "<p style='color:red". $hata;
+
+
+        }
+        if(isset($_SESSION['AdSoyad']))
+        {
+            echo "Hoşgeldiniz" . $_SESSION['Adi'] . " " . $_SESSION['Soyadi'] .
+            " | <a href='?Cikis'> Çıkış Yap </a>";
+
+            else if (isset($_GET['Cikis'])) 
+            {
+                echo "Hoşgeldiniz" .$_SESSION['AdSoyad'].
+                
+                
+            }
+        }
+        $_SESSION['AdSoyad'] ='veli Yılmaz';
+        echo "Hoşgeldiniz" .$_SESSION['AdSoyad'];
         ?>
-    <!-- Code injected by live-server -->
-<script>
-	// <![CDATA[  <-- For SVG support
-	if ('WebSocket' in window) {
-		(function () {
-			function refreshCSS() {
-				var sheets = [].slice.call(document.getElementsByTagName("link"));
-				var head = document.getElementsByTagName("head")[0];
-				for (var i = 0; i < sheets.length; ++i) {
-					var elem = sheets[i];
-					var parent = elem.parentElement || head;
-					parent.removeChild(elem);
-					var rel = elem.rel;
-					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
-						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
-						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
-					}
-					parent.appendChild(elem);
-				}
-			}
-			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
-			var address = protocol + window.location.host + window.location.pathname + '/ws';
-			var socket = new WebSocket(address);
-			socket.onmessage = function (msg) {
-				if (msg.data == 'reload') window.location.reload();
-				else if (msg.data == 'refreshcss') refreshCSS();
-			};
-			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
-				console.log('Live reload enabled.');
-				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
-			}
-		})();
-	}
-	else {
-		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
-	}
-	// ]]>
-</script>
-</body>
+    </body>
 </html>
